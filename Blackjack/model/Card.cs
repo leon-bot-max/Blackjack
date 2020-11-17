1ec02de429b3bf542dd5852cc91190db0a2a046a
L S <61838582+leon-bot-max@users.noreply.github.com>
2020-11-17 10:40:49 +0100

    Add project files.

---
+++ Blackjack/model/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+enum SuitType
+{
+    Club, Diamond, Heart, Spade
+}
+namespace Blackjack.model
+{
+    class Card
+    {
+        public int Value;
+        public SuitType Suit;
+        public int BlackJackValue;
+    }
+}
